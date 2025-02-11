cb267c3ac1ad931ea62309f95798c6a847295ca7
CHxNOBODY <khaopan.nas@gmail.com>
2025-02-11 11:56:27 +0700

    update week1

--- week1/lab3.c
+++ week1/lab3.c
@@ -2,19 +2,15 @@
 
 int main()
 {
-    // กำหนดค่าเริ่มต้น
-    float preGrade = 1.75;       // เกรดเฉลี่ยสะสมที่ผ่านมา
-    int preCredit = 21;          // หน่วยกิตสะสม
-    int credit = 18;             // หน่วยกิตในเทอมนี้
-    float requiredGrade = 2.00;  // เกรดเฉลี่ยรวมที่ต้องการ
+    float preGrade = 1.75;       
+    int preCredit = 21;          
+    int credit = 18;             
+    float requiredGrade = 2.00;  
 
-    // ตัวแปรสำหรับคำนวณเกรดเฉลี่ยเทอมนี้
     float currentSemesterGPA;
 
-    // คำนวณเกรดเฉลี่ยที่ต้องทำในเทอมนี้
     currentSemesterGPA = ((requiredGrade * (preCredit + credit)) - (preGrade * preCredit)) / credit;
 
-    // แสดงผลลัพธ์
     printf("The GPA this semester should be %.2f\n", currentSemesterGPA);
 
     return 0;
