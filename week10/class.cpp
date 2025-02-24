daff4eb03efca63b6c86df57779e1eac9d0c8ba7
CHxNOBODY <khaopan.nas@gmail.com>
2025-02-24 14:30:07 +0700

    update class.cpp

--- week10/class.cpp
+++ week10/class.cpp
@@ -15,35 +15,15 @@ private:           // all attribute are private
     int totalPage; // use lowerCamelCase
     int read;
 public:
-    Book() : Book("Sample Book", 100) {       // default Constructor
-        title = "Sample Book";
-        totalPage = 100;
-        read = 0;
-    }
+    Book();      // default Constructor
+    Book(string t); // This call Constructor chaining
+    Book(string t, int total); // Constructor (Overloading)
 
-    Book(string t) : Book(t, 100) { // This call Constructor chaining
-
-    }
-
-    Book(string t, int total) : read(0) {  // Constructor (Overloading)
-        title = t;
-        totalPage = total;
-    }
-
-    void setTitle(string title) { // method name use lowerCamerCase
-        this -> title = title;
-    }
-    string getTitle() {
-        return title;
-    }
-
-    int getTotalPage() {
-        return totalPage;
-    }
-
-    void readMore(int page) {
-        read += page > 0 ? page : 0;
-    }
+    void setTitle(string title); // method name use lowerCamerCase
+    string getTitle();
+    int getTotalPage();
+    void readMore(int page);
+    string toString();
 };
 
 int main() {
@@ -66,6 +46,47 @@ int main() {
     cout << book2.getTitle() << " " << book2.getTotalPage() << endl;
     cout << b3 -> getTitle() << " " << b3 -> getTotalPage() << endl;
     
+    cout << book1.toString() << endl;
+    cout << book2.toString() << endl;
+    cout << b3 -> toString() << endl;
+
     return 0;
     
+}
+
+void Book::setTitle(string title) {
+    this -> title = title;
+}
+
+string Book::getTitle() {
+    return title;
+    // or you can use "return this -> title"
+}
+
+Book::Book(string title , int totalPage) {
+    this -> title = title;
+    this -> totalPage = totalPage;
+    read = 0;
+}
+
+Book::Book(string title) : Book(title, 200) {}
+
+Book::Book() : Book("Sample Book") {}
+
+int Book::getTotalPage() {
+    return totalPage;
+}
+
+void Book::readMore(int page) {
+    read += page > 0 ? page : 0;
+}
+
+string Book::toString() {
+    string result = "Title";
+    result += title;
+    result += ", read: ";
+    result += to_string(read);
+    result += ", totalPage: ";
+    result += to_string(totalPage);
+    return result;
 }
